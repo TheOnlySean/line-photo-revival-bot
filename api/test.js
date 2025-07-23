67d9a7b786c44b274a7553f28986ceb7684db724
TheOnlySean <sean.xiao@outlook.jp>
2025-07-24 01:21:27 +0900

    remove test api file

--- api/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({ 
-    message: 'Test API works!', 
-    method: req.method,
-    url: req.url,
-    timestamp: new Date().toISOString()
-  });
-}; 
